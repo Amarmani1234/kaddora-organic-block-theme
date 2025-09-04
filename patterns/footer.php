2095a4f21f484225d3cfe90d69bf763bae057034
Chandan Kumar <93089403+chandan578@users.noreply.github.com>
2025-09-04 13:07:44 +0530

    add front page first banner

--- patterns/footer.php
+++ patterns/footer.php
@@ -6,25 +6,21 @@
  */
 ?>
 <!-- wp:group {"style":{"color":{"background":"#f4f4f4"},"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"layout":{"type":"constrained","contentSize":"100%"}} -->
-<div class="wp-block-group has-background"
-    style="background-color:#f4f4f4;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)">
-    <!-- wp:columns -->
-    <div class="wp-block-columns"><!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:site-logo {"width":200,"shouldSyncIcon":true} /-->
+<div class="wp-block-group has-background" style="background-color:#f4f4f4;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--30);padding-bottom:var(--wp--preset--spacing--30)"><!-- wp:columns -->
+<div class="wp-block-columns"><!-- wp:column -->
+<div class="wp-block-column"><!-- wp:site-logo {"width":200,"shouldSyncIcon":true} /-->
 
-            <!-- wp:paragraph {"style":{"color":{"text":"#89969b"},"elements":{"link":{"color":{"text":"#89969b"}}}}} -->
-            <p class="has-text-color has-link-color" style="color:#89969b">Freshness You Can Taste, Quality You Can
-                Trust</p>
-            <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:column -->
-vel":3} -->
-            <h3 class="wp-block-heading">Organic</h3>
-            <!-- /wp:heading -->
+<!-- wp:paragraph {"style":{"color":{"text":"#89969b"},"elements":{"link":{"color":{"text":"#89969b"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#89969b">Freshness You Can Taste, Quality You Can Trust</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column -->
 
-            <!-- wp:navigation {"overlayMenu":"never","style":{"typography":{"fontSize":"1.3rem"}},"layout":{"type":"flex","orientation":"vertical"}} -->
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:heading {"le
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"1.5rem"}}} -->
+<h3 class="wp-block-heading" style="font-size:1.5rem">Organic</h3>
+<!-- /wp:heading -->
+
+<!-- wp:navigation {"overlayMenu":"never","style":{"typography":{"fontSize":"1rem"},"spacing":{"blockGap":"1px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
 <!-- wp:navigation-link {"label":"<?php esc_html_e( 'Home', 'kaddora-organic-block-theme' ); ?>","url":"#"} /-->
 
 <!-- wp:navigation-link {"label":"<?php esc_html_e( 'Service', 'kaddora-organic-block-theme' ); ?>","url":"#"} /-->
@@ -32,16 +28,15 @@ vel":3} -->
 <!-- wp:navigation-link {"label":"<?php esc_html_e( 'About Us', 'kaddora-organic-block-theme' ); ?>","url":"#"} /-->
 
 <!-- wp:navigation-link {"label":"<?php esc_html_e( 'Contact Us', 'kaddora-organic-block-theme' ); ?>","url":"#"} /-->
-<!-- /wp:navigation -->
-        </div>
-        <!-- /wp:column -->
+<!-- /wp:navigation --></div>
+<!-- /wp:column -->
 
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:heading {"level":3} -->
-            <h3 class="wp-block-heading">Quick Link</h3>
-            <!-- /wp:heading -->
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"1.5rem"}}} -->
+<h3 class="wp-block-heading" style="font-size:1.5rem">Quick Link</h3>
+<!-- /wp:heading -->
 
-            <!-- wp:navigation {"overlayMenu":"never","style":{"typography":{"fontSize":"1.3rem"}},"layout":{"type":"flex","orientation":"vertical"}} -->
+<!-- wp:navigation {"ref":569,"overlayMenu":"never","style":{"typography":{"fontSize":"1rem"},"spacing":{"blockGap":"1px"}},"layout":{"type":"flex","orientation":"vertical"}} -->
 <!-- wp:navigation-link {"label":"<?php esc_html_e( 'Offers', 'kaddora-organic-block-theme' ); ?>","url":"#"} /-->
 
 <!-- wp:navigation-link {"label":"<?php esc_html_e( 'Discount Coupons', 'kaddora-organic-block-theme' ); ?>","url":"#"} /-->
@@ -51,42 +46,35 @@ vel":3} -->
 <!-- wp:navigation-link {"label":"<?php esc_html_e( 'New Orders', 'kaddora-organic-block-theme' ); ?>","url":"#"} /-->
 
 <!-- /wp:navigation -->
-        </div>
-        <!-- /wp:column -->
-
-        <!-- wp:column -->
-        <div class="wp-block-column"><!-- wp:heading {"level":3} -->
-            <h3 class="wp-block-heading">Subscribe</h3>
-            <!-- /wp:heading -->
-
-            <!-- wp:paragraph {"style":{"color":{"text":"#89969b"},"elements":{"link":{"color":{"text":"#89969b"}}}}} -->
-            <p class="has-text-color has-link-color" style="color:#89969b">Subscribe to our newsletter</p>
-            <!-- /wp:paragraph -->
-        </div>
-        <!-- /wp:column -->
-    </div>
-    <!-- /wp:columns -->
-
-    <!-- wp:separator {"className":"is-style-wide"} -->
-    <hr class="wp-block-separator has-alpha-channel-opacity is-style-wide" />
-    <!-- /wp:separator -->
-
-    <!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-    <div class="wp-block-group alignfull"
-        style="padding-right:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)">
-        <!-- wp:paragraph {"fontSize":"small"} -->
-        <p class="has-small-font-size">©️ 2025 Kaddoar Tech. All rights reserved.</p>
-        <!-- /wp:paragraph -->
-
-        <!-- wp:paragraph {"fontSize":"small"} -->
-        <p class="has-small-font-size">Made by ❤️ with Theme Kaddora</p>
-        <!-- /wp:paragraph -->
-
-        <!-- wp:paragraph {"fontSize":"small"} -->
-        <p class="has-small-font-size">Designed with <a href="https://kaddora.com/" data-type="link"
-                data-id="https://kaddora.com/" target="_blank" rel="noreferrer noopener">Kaddora Tech</a></p>
-        <!-- /wp:paragraph -->
-    </div>
-    <!-- /wp:group -->
 </div>
+<!-- /wp:column -->
+
+<!-- wp:column -->
+<div class="wp-block-column"><!-- wp:heading {"level":3,"style":{"typography":{"fontSize":"1.5rem"}}} -->
+<h3 class="wp-block-heading" style="font-size:1.5rem">Subscribe</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"color":{"text":"#89969b"},"elements":{"link":{"color":{"text":"#89969b"}}}}} -->
+<p class="has-text-color has-link-color" style="color:#89969b">Subscribe to our newsletter</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+
+<!-- wp:separator {"className":"is-style-wide"} -->
+<hr class="wp-block-separator has-alpha-channel-opacity is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|20","left":"var:preset|spacing|20"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignfull" style="padding-right:var(--wp--preset--spacing--20);padding-left:var(--wp--preset--spacing--20)"><!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">©️ 2025 Kaddoar Tech. All rights reserved.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Made by ❤️ with Theme Kaddora</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"fontSize":"small"} -->
+<p class="has-small-font-size">Designed with <a href="https://kaddora.com/" data-type="link" data-id="https://kaddora.com/" target="_blank" rel="noreferrer noopener">Kaddora Tech</a></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
